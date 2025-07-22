f179a53b0a9368a5b8285c6217a6f13115d0bd4b
ellieene <ellieene@student.21-school.ru>
2025-07-22 18:06:02 +0700

    first commit

---
+++ src/test/java/com/example/CodeArena/CodeArenaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.CodeArena;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodeArenaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
